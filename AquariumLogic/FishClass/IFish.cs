acdfcaf551c4eb38f14a409c3ef942781283c0b0
Simon Rusinov <mrpinkolik@gmail.com>
2018-11-17 00:31:20 +0500

    Added IFood interface Added Velocity field to IFish

--- AquariumLogic/FishClass/IFish.cs
+++ AquariumLogic/FishClass/IFish.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AquariumLogic.FoodClass;
+using System.Numerics;
 
 namespace AquariumLogic.FishClass
 {
@@ -10,7 +12,9 @@ namespace AquariumLogic.FishClass
     {
         double Health { get; }
         double MaxHealth { get; }
+        Vector2 Velocity { get;  }
 
         void StartLiving();
+        void ConsumeFood(IFood food);
     }
 }
