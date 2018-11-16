90a756a6ee992284e9c4cdc76c99f268a0c1aeb9
Simon Rusinov <mrpinkolik@gmail.com>
2018-11-16 22:58:33 +0500

    Added AquariumLogic project Added Aquarium class

---
+++ AquariumLogic/AquariumClass/Aquarium.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquariumLogic.AquariumClass
+{
+    public class Aquarium
+    {
+        public List<object> Fishes { get; set; }
+
+        public Aquarium()
+        {
+            Fishes = new List<object>();
+        }
+
+        public void AddFish()
+        {
+            Fishes.Add(new object());
+        }
+    }
+}
---
+++ AquariumLogic/AquariumClass/Aquarium_Should.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AquariumLogic.AquariumClass
+{
+    [TestFixture]
+    public class Aquarium_Should
+    {
+        private Aquarium aquarium;
+
+        [SetUp]
+        public void SetUp()
+        {
+            aquarium = new Aquarium();
+        }
+
+        [Test]
+        public void NewAquarium_ShouldBeEmpty()
+        {
+            Assert.AreEqual(0, aquarium.Fishes.Count);
+        }
+
+        [Test]
+        public void Aquarium_AddFishWorks()
+        {
+            aquarium.AddFish();
+
+            Assert.AreEqual(1, aquarium.Fishes.Count);
+        }
+    }
+}
