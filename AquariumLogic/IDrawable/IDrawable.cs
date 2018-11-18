a80d6ac5456a9dd3007398655e04baf36cfa209f
Simon Rusinov <mrpinkolik@gmail.com>
2018-11-19 01:00:53 +0500

    Added IDrawable interface

---
+++ AquariumLogic/IDrawable/IDrawable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquariumLogic.IDrawable
+{
+    public interface IDrawable
+    {
+        Size Size { get; }
+        Bitmap Texture { get; }
+    }
+}
