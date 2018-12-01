57814809fa1cfa22ce016581b23898262e356ce3
Simon Rusinov <mrpinkolik@gmail.com>
2018-12-01 22:51:22 +0500

    Added extensions tests

---
+++ AquariumLogic/PointExtensionClass/PointExtension_Should.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AquariumLogic.PointExtensionClass
+{
+    [TestFixture]
+    public class PointExtension_Should
+    {
+        private Point point;
+
+        [SetUp]
+        public void SetUp()
+        {
+            point = new Point(0, 0);
+        }
+
+        [Test]
+        public void CorrectPoint_WhenAddPositiveVector()
+        {
+            var vector = new Vector2(1,3);
+
+            var actual = point.AddVector(vector);
+
+            Assert.AreEqual(new Point((int) (point.X + vector.X), (int) (point.Y + vector.Y)), actual);
+        }
+
+        [Test]
+        public void CorrectPoint_WhenAddNegativeVector()
+        {
+            var vector = new Vector2(-3,-2);
+
+            var actual = point.AddVector(vector);
+
+            Assert.AreEqual(new Point((int) (point.X + vector.X), (int) (point.Y + vector.Y)), actual);
+        }
+
+        [Test]
+        public void CorrectVector_WhenGetVectorToPointCalled()
+        {
+            var newPoint = new Point(3, 4);
+
+            var actual = point.GetVectorToPoint(newPoint);
+
+            var expected = new Vector2(newPoint.X-point.X, newPoint.Y-point.Y); 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ AquariumLogic/SizeExtensionClass/SizeExtension_Should.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AquariumLogic.IDrawableInterface;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AquariumLogic.SizeExtensionClass
+{
+    [TestFixture]
+    public class SizeExtension_Should
+    {
+        private Size size;
+        private Mock<IDrawable> drawable;
+
+        [SetUp]
+        public void SetUp()
+        {
+            size = new Size(10, 10);
+            drawable = new Mock<IDrawable>();
+        }
+
+        [Test]
+        public void ReturnTrue_WhenPointOutOfBounds()
+        {
+            var point = new Point(11, 9);
+
+            var actual = size.IsOutOfBounds(point);
+
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void ReturnFalse_WhenPointInsideOfBounds()
+        {
+            var point = new Point(3, 9);
+
+            var actual = size.IsOutOfBounds(point);
+
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void ReturnTrue_WhenDrawableOutOfBounds()
+        {
+            var drawableSize = new Size(4, 3);
+            var drawablePos = new Point(10, 10);
+            drawable.Setup(d => d.Size).Returns(drawableSize);
+            drawable.Setup(d => d.Position).Returns(drawablePos);
+
+            var actual = size.IsOutOfBounds(drawable.Object);
+
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void ReturnTrue_WhenDrawableLargerThanContainer()
+        {
+            var drawableSize = new Size(20, 20);
+            var drawablePos = new Point(0, 0);
+            drawable.Setup(d => d.Size).Returns(drawableSize);
+            drawable.Setup(d => d.Position).Returns(drawablePos);
+
+            var actual = size.IsOutOfBounds(drawable.Object);
+
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void ReturnFalse_WhenDrawableInsideOfBounds()
+        {
+            var drawableSize = new Size(4, 3);
+            var drawablePos = new Point(0, 0);
+            drawable.Setup(d => d.Size).Returns(drawableSize);
+            drawable.Setup(d => d.Position).Returns(drawablePos);
+
+            var actual = size.IsOutOfBounds(drawable.Object);
+
+            Assert.False(actual);
+        }
+    }
+}
